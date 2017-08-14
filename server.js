e4a995482016955762a89bb85b68ba241d318d87
James Langford <derivative7117@live.com>
2017-08-14 15:05:08 -0600

    added event listeners

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     app = express(),
     path = require('path'),
-    port = 3004;
+    port = 3010;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
