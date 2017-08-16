17ec8ddc6f12a5d449a040f7cf2088e2047e11e1
James Langford <derivative7117@live.com>
2017-08-16 10:17:36 -0600

    edited for salesforce

--- public/js/map.js
+++ public/js/map.js
@@ -1,23 +1,14 @@
 function initMap() {
-    var address = {lat: 40.761, lng: -111.891};
-        //"{!SUBSTITUTE(JSENCODE(Property__c.Address__c),'\n',' ')} {!SUBSTITUTE(JSENCODE(Property__c.City__c),'\n',' ')} {!SUBSTITUTE(JSENCODE(Property__c.State__c),'\n',' ')}";
-    var infowindow = new google.maps.InfoWindow({
-        content: "<b>{!Property__c.Address__c}</b><br>" + address + " "
-    });
+    var myLatLng = {lat: -25.363, lng: 131.044};
+
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: address
+        zoom: 4,
+        center: myLatLng
     });
+
     var marker = new google.maps.Marker({
-        position: address,
+        position: myLatLng,
         map: map,
-        title: "hello man"
-            //"{!Property__c.Address__c}"
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-    });
-    google.maps.event.addListener(infowindow, 'closeclick', function() {
-        map.setCenter(marker.getPosition());
+        title: 'Hello World!'
     });
 }
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express'),
     app = express(),
     path = require('path'),
-    port = 3010;
+    port = 3011;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/api', (req, res) => {
-  res.status(200).send('hey man');
-})
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
